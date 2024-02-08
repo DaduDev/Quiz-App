23ca1897144ed3dc4251ae10189cfc60e2d05308
Shaik Dadapeer <69897636+DaduDev@users.noreply.github.com>
2024-02-08 21:22:17 +0530

    worked on create page

--- src/components/Extra/Create/Create.js
+++ src/components/Extra/Create/Create.js
@@ -1,29 +1,19 @@
 import React from "react";
 import Header from "../../Header/Header";
+import "./Create.css";
 
 const Create = () => {
   return (
     <div>
       <Header />
-      <div>
-        <div>
-          <h3>List of Questions</h3>
-        </div>
-        <div>
-          <h1>Create</h1>
-          <div>
-            <input placeholder="Add your question here" />
-            <div>
-              <input placeholder="Option-01" />
-              <input placeholder="Option-02" />
-              <input placeholder="Option-03" />
-              <input placeholder="Option-04" />
-            </div>
-          </div>
-          <div>
-            <button></button>
-          </div>
-        </div>
+      <div className="createQuiz">
+        <h2>Name of the Quiz</h2>
+        <input
+          className="intpTag"
+          type="text"
+          placeholder="Enter your quiz name"
+        />
+        <button className="btn">Next</button>
       </div>
     </div>
   );
